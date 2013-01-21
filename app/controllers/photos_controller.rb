960542e832800337a78d0c0f18fbe5640dff10aa
Adam Miribyan <adam.miribyan@gmail.com>
2013-01-21 21:01:21 +0100

    Synchronize by GET photos/geronimo.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,4 +2,10 @@ class PhotosController < ApplicationController
   def index
     @photos = Photo.all
   end
+
+  def geronimo
+    @fetcher.synchronize! if @fetcher.remote_modified?
+
+    redirect_to photos_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Amongster::Application.routes.draw do
-  resources :photos, only: [:index]
+  resources :photos, only: [:index] do
+    get :geronimo, on: :collection
+  end
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
